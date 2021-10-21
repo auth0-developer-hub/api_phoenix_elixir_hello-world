0776b86f78257cd5e60b33f22bb697ac501d37bb
Manu S Ajith <neo@codingarena.in>
2021-10-22 02:06:28 +0300

    Fix error messages
    Signed-off-by: Manu S Ajith <neo@codingarena.in>

--- lib/hello_world/auth/authenticate.ex
+++ lib/hello_world/auth/authenticate.ex
@@ -17,11 +17,17 @@ defmodule HelloWorld.Auth.Authenticate do
   """
   @spec call(Plug.Conn.t(), any) :: Plug.Conn.t()
   def call(conn, _default) do
-    with token when is_binary(token) <- get_token(conn),
+    with {:ok, token} when is_binary(token) <- get_token(conn),
          {:ok, _claims} <- Token.verify_and_validate(token) do
       conn
     else
-      _ ->
+      {:error, :missing_token} ->
+        conn
+        |> put_status(401)
+        |> json(%{message: "No authorization token was found"})
+        |> halt
+
+      _error ->
         conn
         |> put_status(401)
         |> json(%{message: "Unauthorized"})
@@ -31,9 +37,9 @@ defmodule HelloWorld.Auth.Authenticate do
 
   defp get_token(conn) do
     case get_req_header(conn, "authorization") do
-      ["Bearer " <> token] -> token
-      [token] -> token
-      _ -> nil
+      ["Bearer " <> token] -> {:ok, token}
+      [token] -> {:ok, token}
+      _ -> {:error, :missing_token}
     end
   end
 end
