4fa6d63232d25a0c37790feb30b0f0f344585c84
Byron Motoche <bmotoche@stackbuilders.com>
2022-01-19 10:25:44 -0500

    fix missing headers on 404 responses

--- lib/hello_world_web/controllers/api/message_controller.ex
+++ lib/hello_world_web/controllers/api/message_controller.ex
@@ -1,25 +1,23 @@
 defmodule HelloWorldWeb.API.MessageController do
   use HelloWorldWeb, :controller
 
-  defp metadata() do
-    %{ api: "api_phoenix_elixir_hello-world", branch: "starter"}
-  end
+  @metadata %{ api: "api_phoenix_elixir_hello-world", branch: "starter"}
 
   @spec public(Plug.Conn.t(), any) :: Plug.Conn.t()
   def public(conn, _params) do
     text = "This is a public message."
-    json(conn, %{metadata: metadata(), text: text})
+    json(conn, %{metadata: @metadata, text: text})
   end
 
   @spec protected(Plug.Conn.t(), any) :: Plug.Conn.t()
   def protected(conn, _params) do
     text = "This is a protected message."
-    json(conn, %{metadata: metadata(), text: text})
+    json(conn, %{metadata: @metadata, text: text})
   end
 
   @spec admin(Plug.Conn.t(), any) :: Plug.Conn.t()
   def admin(conn, _params) do
     text = "This is an admin message."
-    json(conn, %{metadata: metadata(), text: text})
+    json(conn, %{metadata: @metadata, text: text})
   end
 end
